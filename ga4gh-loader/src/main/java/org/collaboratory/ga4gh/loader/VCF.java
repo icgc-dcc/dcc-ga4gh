63e580a5d65d5af3669ab9df4ec5db883e012175
Robert Tisma <robert.tisma@oicr.on.ca>
2016-11-22 16:28:08 -0500

    using JsonNodeBuilders instead

--- ga4gh-loader/src/main/java/org/collaboratory/ga4gh/loader/VCF.java
+++ ga4gh-loader/src/main/java/org/collaboratory/ga4gh/loader/VCF.java
@@ -1,7 +1,7 @@
 package org.collaboratory.ga4gh.loader;
 
 import static com.google.common.collect.Iterables.transform;
-import static org.icgc.dcc.common.core.json.Jackson.DEFAULT;
+import static org.icgc.dcc.common.core.json.JsonNodeBuilders.object;
 
 import java.io.Closeable;
 import java.io.File;
@@ -13,7 +13,6 @@ import htsjdk.variant.vcf.VCFEncoder;
 import htsjdk.variant.vcf.VCFFileReader;
 import htsjdk.variant.vcf.VCFHeader;
 import lombok.NonNull;
-import lombok.val;
 
 public class VCF implements Closeable {
 
@@ -41,19 +40,18 @@ public class VCF implements Closeable {
   }
 
   private ObjectNode convert(VariantContext record) {
-    val variantDoc = DEFAULT.createObjectNode();
-    variantDoc.put("id", record.getID());
-    variantDoc.put("variant_set_id", "test");
-    variantDoc.put("start", record.getStart());
-    variantDoc.put("end", record.getEnd());
-    variantDoc.put("reference_name", record.getContig());
-    variantDoc.put("record", encoder.encode(record));
-    variantDoc.put("call_set_id", this.additionalSourceData.getObjectId());
-    variantDoc.put("file_id", this.additionalSourceData.getFileId());
-    variantDoc.put("donor_id", this.additionalSourceData.getDonorId());
-    variantDoc.put("sample_id", this.additionalSourceData.getSampleId());
-
-    return variantDoc;
+    return object()
+        .with("id", record.getID())
+        .with("variant_set_id", "test")
+        .with("start", record.getStart())
+        .with("end", record.getEnd())
+        .with("reference_name", record.getContig())
+        .with("record", encoder.encode(record))
+        .with("call_set_id", this.additionalSourceData.getObjectId())
+        .with("file_id", this.additionalSourceData.getFileId())
+        .with("donor_id", this.additionalSourceData.getDonorId())
+        .with("sample_id", this.additionalSourceData.getSampleId())
+        .end();
   }
 
   @Override
