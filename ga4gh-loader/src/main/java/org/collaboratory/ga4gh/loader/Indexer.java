1cb022bf2f500ea40cc1be1f2c795b28c531db57
Robert Tisma <robert.tisma@oicr.on.ca>
2017-01-04 12:27:00 -0500

    typo

--- ga4gh-loader/src/main/java/org/collaboratory/ga4gh/loader/Indexer.java
+++ ga4gh-loader/src/main/java/org/collaboratory/ga4gh/loader/Indexer.java
@@ -45,7 +45,7 @@ public class Indexer {
   public static final String VARIANT_TYPE_NAME = "variant";
   public static final String VCF_HEADER_TYPE_NAME = "vcf_header";
   private static final ObjectWriter BINARY_WRITER = JacksonFactory.getObjectWriter();
-  private static final String MAPPINGS_DIR = "org/collaboratory/ga4gh/common/mappings";
+  private static final String MAPPINGS_DIR = "org/collaboratory/ga4gh/resources/mappings";
 
   /**
    * Dependencies.
