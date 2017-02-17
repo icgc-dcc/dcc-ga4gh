43619326035e198bc7570b39d08357a8aa95630c
Robert Tisma <robert.tisma@oicr.on.ca>
2017-02-17 12:52:24 -0500

    used Cleanup annotation for ObjectPersistance streams

--- ga4gh-core/src/main/java/org/collaboratory/ga4gh/core/ObjectPersistance.java
+++ ga4gh-core/src/main/java/org/collaboratory/ga4gh/core/ObjectPersistance.java
@@ -1,6 +1,8 @@
 package org.collaboratory.ga4gh.core;
 
+import lombok.Cleanup;
 import lombok.NoArgsConstructor;
+import lombok.val;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -14,32 +16,17 @@ import static lombok.AccessLevel.PRIVATE;
 public class ObjectPersistance {
 
   public static void store(final Object object, final String filename) throws IOException {
-    FileOutputStream fout = null;
-    ObjectOutputStream oos = null;
-    fout = new FileOutputStream(filename);
-    oos = new ObjectOutputStream(fout);
+    @Cleanup
+    val fout = new FileOutputStream(filename);
+    val oos = new ObjectOutputStream(fout);
     oos.writeObject(object);
-    if (fout != null) {
-      fout.close();
-    }
-    if (oos != null) {
-      oos.close();
-    }
   }
 
   public static Object restore(final String filename) throws ClassNotFoundException, IOException {
-    FileInputStream fin = null;
-    ObjectInputStream ois = null;
-    fin = new FileInputStream(filename);
-    ois = new ObjectInputStream(fin);
-    Object object = ois.readObject();
-    if (fin != null) {
-      fin.close();
-    }
-    if (ois != null) {
-      ois.close();
-    }
-    return object;
+    @Cleanup
+    val fin = new FileInputStream(filename);
+    val ois = new ObjectInputStream(fin);
+    return ois.readObject();
   }
 
 }
