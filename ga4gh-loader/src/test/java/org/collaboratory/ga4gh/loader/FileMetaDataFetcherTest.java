53ecc2b1bccdc4ac4e0313c78ac0e5187510d82d
Robert Tisma <robert.tisma@oicr.on.ca>
2017-02-21 10:51:54 -0500

    added new fileMEtaDataFetcher tests

--- ga4gh-loader/src/test/java/org/collaboratory/ga4gh/loader/FileMetaDataFetcherTest.java
+++ ga4gh-loader/src/test/java/org/collaboratory/ga4gh/loader/FileMetaDataFetcherTest.java
@@ -17,115 +17,188 @@
  */
 package org.collaboratory.ga4gh.loader;
 
+import com.google.common.base.Stopwatch;
 import com.google.common.collect.Sets;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
-import org.collaboratory.ga4gh.core.ObjectPersistance;
 import org.collaboratory.ga4gh.loader.model.metadata.FileMetaDataContext;
-import org.collaboratory.ga4gh.loader.model.metadata.FileMetaDataFetcher;
+import org.collaboratory.ga4gh.loader.model.metadata.fetcher.decorators.OrderFetcherDecorator;
+import org.junit.Ignore;
 import org.junit.Test;
 
-import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.icgc.dcc.common.core.util.Joiners.NEWLINE;
+import static org.collaboratory.ga4gh.loader.model.metadata.fetcher.impl.AllFetcher.newAllFetcherDefaultStorageFilename;
+import static org.collaboratory.ga4gh.loader.model.metadata.fetcher.impl.NumDonorsFetcher.newNumDonorsFetcher;
 
 @Slf4j
 public class FileMetaDataFetcherTest {
 
   @Test
-  public void testSerializer() throws ClassNotFoundException, IOException {
-    int numDonors = 30;
-    val dataFetcherShuffle1 = FileMetaDataFetcher.builder()
-        .numDonors(numDonors)
-        .somaticSSMsOnly(true)
-        .shuffle(true)
-        .build();
-    val fileMetaDataContextOrig = dataFetcherShuffle1.fetch();
-    val filename = "target/fileMetaDatas.testSerializer.bin";
-    fileMetaDataContextOrig.store(filename);
-    val fileMetaDataContextNew = FileMetaDataContext.restore(filename);
-    val setOrig = Sets.newHashSet(fileMetaDataContextOrig);
-    val setNew = Sets.newHashSet(fileMetaDataContextNew);
-    assertThat(setOrig).containsAll(setNew);
-    assertThat(setNew).containsAll(setOrig);
-    log.info("SERIALIZER_TEST_OLD: \n{}", NEWLINE.join(fileMetaDataContextOrig));
-    log.info("SERIALIZER_TEST_NEW: \n{}", NEWLINE.join(fileMetaDataContextNew));
-  }
-
-  @Test
-  public void testRestore() throws IOException, ClassNotFoundException {
+  @SneakyThrows
+  public void testPersistance() {
     final int numDonors = 8;
-    val filename = "target/fileMetaDatas.testRestore.bin";
-    val dataFetcherShuffle1 = FileMetaDataFetcher.builder()
-        .numDonors(numDonors)
-        .somaticSSMsOnly(true)
-        .shuffle(true)
-        .build();
-    val fileMetaDatasOrig = dataFetcherShuffle1.fetch();
-    ObjectPersistance.store(fileMetaDatasOrig, filename);
+    val filename = "target/fileMetaDatas.testPersistance.bin";
+    val fetcher = newNumDonorsFetcher(numDonors);
+    val ctxOriginal = fetcher.fetch();
+    ctxOriginal.store(filename);
 
-    val dataFetcherRestore = FileMetaDataFetcher.builder()
-        .storageFilename(filename)
-        .build();
-    val fileMetaDatasNew = dataFetcherRestore.fetch();
+    val ctxRestored = FileMetaDataContext.restore(filename);
+    verifyFileMetaDataContextsContents(ctxOriginal, ctxRestored);
+  }
 
-    val setOrig = Sets.newHashSet(fileMetaDatasOrig);
-    val setNew = Sets.newHashSet(fileMetaDatasNew);
-    assertThat(setOrig).containsAll(setNew);
-    assertThat(setNew).containsAll(setOrig);
-    log.info("RESTORE_TEST_OLD: \n{}", NEWLINE.join(fileMetaDatasOrig));
-    log.info("RESTORE_TEST_NEW: \n{}", NEWLINE.join(fileMetaDatasNew));
+  /**
+   * This test takes several minutes, so ignoring since will make tests really really long
+   */
+  @Test
+  @SneakyThrows
+  @Ignore
+  public void testAllFetcher() {
+    boolean FORCE_NEW_STORAGE_FILE = true;
+    val fetcher1 = newAllFetcherDefaultStorageFilename(FORCE_NEW_STORAGE_FILE);
+    val watch1 = Stopwatch.createStarted();
+    val ctx1 = fetcher1.fetch();
+    watch1.stop();
 
+    val fetcher2 = newAllFetcherDefaultStorageFilename(! FORCE_NEW_STORAGE_FILE);
+    val watch2 = Stopwatch.createStarted();
+    val ctx2 = fetcher2.fetch();
+    watch2.stop();
+
+    verifyFileMetaDataContextsContents(ctx1, ctx2);
+    assertThat(watch1.elapsed(TimeUnit.SECONDS)).isGreaterThan(watch2.elapsed(TimeUnit.SECONDS)); //Not the best test, but should be considerable speed increase when reading from file.
   }
 
   @Test
-  public void testShuffler() throws ClassNotFoundException, IOException {
-    int numDonors = 30;
-    val dataFetcherShuffle1 = FileMetaDataFetcher.builder()
-        .numDonors(numDonors)
-        .somaticSSMsOnly(true)
-        .shuffle(true)
-        .build();
-    val dataFetcherShuffle2 = FileMetaDataFetcher.builder()
-        .numDonors(numDonors)
-        .somaticSSMsOnly(true)
-        .shuffle(true)
-        .build();
-    val dataFetcherShuffle2_withSeed2 = FileMetaDataFetcher.builder()
-        .numDonors(numDonors)
-        .somaticSSMsOnly(true)
-        .seed(dataFetcherShuffle2.getSeed())
-        .shuffle(true)
-        .build();
+  @SneakyThrows
+  public void testNumDonorsFetcher() {
+    val numDonors = 8;
+    val fetcher1 = newNumDonorsFetcher(numDonors);
+    val ctx1 = fetcher1.fetch();
+    val fetcher2 = newNumDonorsFetcher(numDonors);
+    val ctx2 = fetcher2.fetch();
+    verifyFileMetaDataContextsContents(ctx1, ctx2);
+  }
 
-    val dataFetcherNonShuffle1 = FileMetaDataFetcher.builder()
-        .numDonors(numDonors)
-        .somaticSSMsOnly(true)
-        .shuffle(false)
-        .build();
-    val dataFetcherNonShuffle2 = FileMetaDataFetcher.builder()
-        .numDonors(numDonors)
-        .somaticSSMsOnly(true)
-        .shuffle(false)
-        .build();
+  private void verifyFileMetaDataContextsContents(final FileMetaDataContext ctx1, final FileMetaDataContext ctx2){
+    assertThat(ctx1.size() > 0);
+    assertThat(ctx2.size() > 0);
+    val set1 = Sets.newHashSet(ctx1);
+    val set2 = Sets.newHashSet(ctx2);
+    assertThat(set1.size()).isEqualTo(ctx1.size());
+    assertThat(set2.size()).isEqualTo(ctx2.size());
+    assertThat(set1.containsAll(set2));
+    assertThat(set2.containsAll(set1));
+  }
 
-    val fmdShuffle1 = dataFetcherShuffle1.fetch();
-    val fmdShuffle2 = dataFetcherShuffle2.fetch();
-    val fmdShuffle2_withSeed2 = dataFetcherShuffle2_withSeed2.fetch();
-    val fmdNonShuffle1 = dataFetcherNonShuffle1.fetch();
-    val fmdNonShuffle2 = dataFetcherNonShuffle2.fetch();
+  @Test
+  @SneakyThrows
+  public void testOrderingFileSizeAscending()  {
+    orderFileSizeTest(8, true);
+  }
 
-    // Assert that 2 dataFetchers with same parameters except for SEED values, and shuffle ON,
-    // result in FileMetaData lists that are DIFFERENT, therefore NOT EQUAL
-    assertThat(fmdShuffle1).isNotEqualTo(fmdNonShuffle2);
+  @Test
+  @SneakyThrows
+  public void testOrderingFileSizeDescending()  {
+    orderFileSizeTest(8, false);
+  }
 
-    // Assert that 2 dataFetchers with same parameters, regardless of SEED values, and shuffle OFF,
-    // result in FileMetaData lists that are the SAME, therefore EQUAL
-    assertThat(fmdNonShuffle1).isEqualTo(fmdNonShuffle2);
 
-    // Assert that 2 dataFetchers with same parameters, included same SEED values, and shuffle ON,
-    // result in FileMetaData lists that are the SAME, therefore EQUAL
-    assertThat(fmdShuffle2).isEqualTo(fmdShuffle2_withSeed2);
+  @SneakyThrows
+  private void orderFileSizeTest(final int numDonors, final boolean sortAscending ) {
+    val fetcher = newNumDonorsFetcher(numDonors);
+    val orderDecorator = OrderFetcherDecorator.newSizeSortingFetcherDecorator(fetcher, sortAscending);
+    boolean first = true;
+    long fileSize = -1;
+    long prevFileSize = -1;
+    val ctx  = orderDecorator.fetch();
+    for (val f : ctx){
+      prevFileSize = fileSize;
+      fileSize = f.getFileSize();
+      if (first){
+        first = false;
+      } else if (! sortAscending){
+        assertThat(fileSize).isLessThanOrEqualTo(prevFileSize);
+      } else if (sortAscending){
+        assertThat(fileSize).isGreaterThan(prevFileSize);
+      } else {
+        throw new IllegalStateException("There should be no other option");
+      }
+    }
+  }
+
+  @Test
+  @SneakyThrows
+  public void testShufflingSameSeed()  {
+    val numDonors = 8;
+    val fetcher = newNumDonorsFetcher(numDonors);
+    val seed = OrderFetcherDecorator.generateSeed();
+    val orderDecorator1 = OrderFetcherDecorator.newShuffleFetcherDecoratorWithSeed(fetcher, seed );
+    val orderDecorator2 = OrderFetcherDecorator.newShuffleFetcherDecoratorWithSeed(fetcher, seed );
+    val ctx1 = orderDecorator1.fetch();
+    val ctx2 = orderDecorator2.fetch();
+    verifyFileMetaDataContextsContents(ctx1, ctx2);
+    val size = ctx1.size();
+
+    for (int i=0; i< size; i++){
+     val e1 =  ctx1.getFileMetaDatas().get(i);
+     val e2 =  ctx2.getFileMetaDatas().get(i);
+     assertThat(e1.equals(e2));
+    }
+  }
+
+  @Test
+  @SneakyThrows
+  public void testShufflingDifferentSeed()  {
+    val numDonors = 8;
+    val fetcher = newNumDonorsFetcher(numDonors);
+    val seed1 = OrderFetcherDecorator.generateSeed();
+    val seed2 = seed1 - 31984;
+    val orderDecorator1 = OrderFetcherDecorator.newShuffleFetcherDecoratorWithSeed(fetcher, seed1 );
+    val orderDecorator2 = OrderFetcherDecorator.newShuffleFetcherDecoratorWithSeed(fetcher, seed2 );
+    val ctx1 = orderDecorator1.fetch();
+    val ctx2 = orderDecorator2.fetch();
+    verifyFileMetaDataContextsContents(ctx1, ctx2);
+    val size = ctx1.size();
+
+    int numMismatches = 0;
+    for (int i=0; i< size; i++){
+      val e1 =  ctx1.getFileMetaDatas().get(i);
+      val e2 =  ctx2.getFileMetaDatas().get(i);
+      if (!e1.equals(e2)){
+        numMismatches++;
+      }
+    }
+    assertThat(numMismatches).isGreaterThan(2);
+  }
+
+  @Test
+  @SneakyThrows
+  public void testOrderFilename(){
+    val numDonors = 8;
+    val fetcher = newNumDonorsFetcher(numDonors);
+    val sortAscending = true;
+
+    // Shuffle one fetcher, then sort
+    val shuffleDecorator1 = OrderFetcherDecorator.newShuffleFetcherDecorator(fetcher);
+    val orderDecorator1 = OrderFetcherDecorator.newFilenameSortingFetcherDecorator(shuffleDecorator1, sortAscending);
+
+    // Shuffle second fetcher, then sort
+    val shuffleDecorator2 = OrderFetcherDecorator.newShuffleFetcherDecorator(fetcher);
+    val orderDecorator2 = OrderFetcherDecorator.newFilenameSortingFetcherDecorator(shuffleDecorator2, sortAscending);
+
+    // Then compare them, and order should be preserved
+    val ctx1 = orderDecorator1.fetch();
+    val ctx2 = orderDecorator2.fetch();
+    verifyFileMetaDataContextsContents(ctx1, ctx2);
+
+    val size = ctx1.size();
+    for (int i=0; i< size; i++){
+      val e1 =  ctx1.getFileMetaDatas().get(i);
+      val e2 =  ctx2.getFileMetaDatas().get(i);
+      assertThat(e1.equals(e2));
+    }
   }
 }
