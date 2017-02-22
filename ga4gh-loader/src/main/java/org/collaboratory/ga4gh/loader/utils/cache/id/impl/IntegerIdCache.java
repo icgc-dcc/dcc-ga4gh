65562e0c728072a00bee8487f876706d7d367824
Robert Tisma <robert.tisma@oicr.on.ca>
2017-02-22 15:07:39 -0500

    updated

--- ga4gh-loader/src/main/java/org/collaboratory/ga4gh/loader/utils/cache/id/impl/IntegerIdCache.java
+++ ga4gh-loader/src/main/java/org/collaboratory/ga4gh/loader/utils/cache/id/impl/IntegerIdCache.java
@@ -1,12 +1,13 @@
 package org.collaboratory.ga4gh.loader.utils.cache.id.impl;
 
-import lombok.val;
 import org.collaboratory.ga4gh.loader.utils.cache.id.AbstractIdCacheTemplate;
 import org.collaboratory.ga4gh.loader.utils.cache.storage.CacheStorage;
 
 import static com.google.common.base.Preconditions.checkState;
 
 public class IntegerIdCache<K> extends AbstractIdCacheTemplate<K, Integer> {
+
+  private static final Integer SINGLE_INCREMENT_AMOUNT = 1;
 
   public static <K> IntegerIdCache<K> newIntegerIdCache(final CacheStorage<K, Integer> cache, Integer id) {
     return new IntegerIdCache<K>(cache, id);
@@ -18,21 +19,21 @@ public class IntegerIdCache<K> extends AbstractIdCacheTemplate<K, Integer> {
 
   @Override
   protected void checkIdLowerBound() {
-    val count = getCount();
+    final Integer count = getCount();
     checkState(count >= Integer.MIN_VALUE, "The id %d must be >= %d", count, Integer.MIN_VALUE);
   }
 
   @Override
   protected void checkIdUpperBound() {
-    val count = getCount();
+    final Integer count = getCount();
     checkState(count < Integer.MAX_VALUE, "The id %d must be < %d", count, Integer.MAX_VALUE);
 
   }
 
   @Override
   protected Integer incr() {
-    val out = getCount();
-    setCount(getCount() + 1);
+    final Integer out = getCount();
+    setCount(getCount() + SINGLE_INCREMENT_AMOUNT);
     return out;
   }
 
--- ga4gh-loader/src/main/java/org/collaboratory/ga4gh/loader/utils/cache/id/impl/LongIdCache.java
+++ ga4gh-loader/src/main/java/org/collaboratory/ga4gh/loader/utils/cache/id/impl/LongIdCache.java
@@ -1,12 +1,13 @@
 package org.collaboratory.ga4gh.loader.utils.cache.id.impl;
 
-import lombok.val;
 import org.collaboratory.ga4gh.loader.utils.cache.id.AbstractIdCacheTemplate;
 import org.collaboratory.ga4gh.loader.utils.cache.storage.CacheStorage;
 
 import static com.google.common.base.Preconditions.checkState;
 
 public class LongIdCache<K> extends AbstractIdCacheTemplate<K, Long> {
+
+  private static final Long SINGLE_INCREMENT_AMOUNT = 1L;
 
   public static <K> LongIdCache<K> newLongIdCache(final CacheStorage<K, Long> cacheStorage, final Long id) {
     return new LongIdCache<K>(cacheStorage, id);
@@ -18,21 +19,21 @@ public class LongIdCache<K> extends AbstractIdCacheTemplate<K, Long> {
 
   @Override
   protected void checkIdLowerBound() {
-    val count = getCount();
+    final Long count = getCount();
     checkState(count >= Long.MIN_VALUE, "The id %d must be >= %d", count, Long.MIN_VALUE);
   }
 
   @Override
   protected void checkIdUpperBound() {
-    val count = getCount();
+    final Long count = getCount();
     checkState(count < Long.MAX_VALUE, "The id %d must be < %d", count, Long.MAX_VALUE);
 
   }
 
   @Override
   protected Long incr() {
-    val out = getCount();
-    setCount(getCount() + 1);
+    final Long out = getCount();
+    setCount(getCount() + SINGLE_INCREMENT_AMOUNT);
     return out;
   }
 
