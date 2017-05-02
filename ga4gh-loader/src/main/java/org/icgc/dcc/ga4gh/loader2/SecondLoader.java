46b528e10bfbdb6afb3aaf946ffee4043c3c55ed
rtisma <rtisma@gmail.com>
2017-05-02 19:05:44 -0400

    updated secondLoader

--- ga4gh-loader/src/main/java/org/icgc/dcc/ga4gh/loader2/SecondLoader.java
+++ ga4gh-loader/src/main/java/org/icgc/dcc/ga4gh/loader2/SecondLoader.java
@@ -6,13 +6,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.icgc.dcc.ga4gh.common.model.converters.EsVariantConverterJson2;
+import org.icgc.dcc.ga4gh.common.model.es.EsCall;
 import org.icgc.dcc.ga4gh.common.types.WorkflowTypes;
+import org.icgc.dcc.ga4gh.loader2.callconverter.CallConverterStrategyMux;
 import org.icgc.dcc.ga4gh.loader2.persistance.FileObjectRestorerFactory;
 import org.icgc.dcc.ga4gh.loader2.storage.StorageFactory;
 
 import java.nio.file.Paths;
 
 import static org.icgc.dcc.common.core.util.Joiners.NEWLINE;
+import static org.icgc.dcc.ga4gh.common.model.converters.EsVariantSetConverterJson.convertFromPortalMetadata;
 import static org.icgc.dcc.ga4gh.common.model.es.EsVcfHeader.createEsVcfHeader;
 import static org.icgc.dcc.ga4gh.loader.Config.CALL_SET_ID_STORAGE_DB_PATH;
 import static org.icgc.dcc.ga4gh.loader.Config.STORAGE_OUTPUT_VCF_STORAGE_DIR;
@@ -22,6 +26,7 @@ import static org.icgc.dcc.ga4gh.loader.Config.VARIANT_SET_ID_STORAGE_DB_PATH;
 import static org.icgc.dcc.ga4gh.loader.utils.CounterMonitor.newMonitor;
 import static org.icgc.dcc.ga4gh.loader2.CallSetIdStorageFactory.createCallSetIdStorageFactory;
 import static org.icgc.dcc.ga4gh.loader2.PreProcessor.createPreProcessor;
+import static org.icgc.dcc.ga4gh.loader2.CallSetDao.createCallSetDao;
 import static org.icgc.dcc.ga4gh.loader2.VariantSetIdStorageFactory.createVariantSetIdStorageFactory;
 import static org.icgc.dcc.ga4gh.loader2.dao.portal.PortalMetadataDaoFactory.newDefaultPortalMetadataDaoFactory;
 import static org.icgc.dcc.ga4gh.loader2.portal.PortalCollabVcfFileQueryCreator.newPortalCollabVcfFileQueryCreator;
@@ -30,6 +35,9 @@ import static org.icgc.dcc.ga4gh.loader2.utils.VCF.newDefaultVCFFileReader;
 @Slf4j
 public class SecondLoader {
   private static final boolean F_CHECK_CORRECT_WORKFLOW_TYPE = false;
+  private static final CallConverterStrategyMux CALL_CONVERTER_STRATEGY_MUX = new CallConverterStrategyMux();
+
+  private static final EsVariantConverterJson2 ES_VARIANT_CONVERTER_JSON_2 = new EsVariantConverterJson2();
 
   public static void main(String[] args){
     val storage = StorageFactory.builder()
@@ -50,22 +58,44 @@ public class SecondLoader {
     val preProcessor = createPreProcessor(portalMetadataDao, variantSetIdStorageFactory, callSetIdStorageFactory);
     preProcessor.init();
 
+    val variantSetIdStorage = preProcessor.getVariantSetIdStorage();
+    val callSetIdStorage = preProcessor.getCallSetIdStorage();
+    val callSetDao = createCallSetDao(callSetIdStorage);
+
     long numVariants = 0;
     int count = 0;
     val total = portalMetadataDao.findAll().size();
     val counterMonitor = newMonitor("call", 2000000);
     counterMonitor.start();
     for (val portalMetadata : portalMetadataDao.findAll()){
+
       try{
-        val file = storage.getFile(portalMetadata);
-        log.info("Downloaded [{}/{}]: {}", ++count, total, portalMetadata.getPortalFilename().getFilename());
         val workflow = portalMetadata.getPortalFilename().getWorkflow();
         val workflowType = WorkflowTypes.parseMatch(workflow, F_CHECK_CORRECT_WORKFLOW_TYPE);
+        val file = storage.getFile(portalMetadata);
+
+        val variantSet = convertFromPortalMetadata(portalMetadata);
+        val variantSetId = variantSetIdStorage.getId(variantSet);
+
+        val callSet = callSetDao.find(portalMetadata).get();
+        val callSetName = callSet.getName();
+        val callSetId = callSetIdStorage.getId(callSet);
+
+        log.info("Downloaded [{}/{}]: {}", ++count, total, portalMetadata.getPortalFilename().getFilename());
         val vcfFileReader = newDefaultVCFFileReader(file);
         val vcfFileHeader = vcfFileReader.getFileHeader();
         val esVcfHeader = createEsVcfHeader(portalMetadata, vcfFileHeader);
+        val callConverter = CALL_CONVERTER_STRATEGY_MUX.select(portalMetadata);
+
+        val esCallBuilder = EsCall.builder()
+            .variantSetId(variantSetId)
+            .callSetName(callSetName)
+            .callSetId(callSetId);
+
         for (val variant : vcfFileReader){
-          numVariants++;
+          val esCalls = callConverter.convert(esCallBuilder, variant);
+          val esVariant2 = ES_VARIANT_CONVERTER_JSON_2.convertFromVariantContext(variant);
+          esCalls.forEach(esVariant2::addCall);
         }
       } catch (Exception e){
         log.error("Exception [{}]: {}\n{}", e.getClass().getName(), e.getMessage(), NEWLINE.join(e.getStackTrace()));
