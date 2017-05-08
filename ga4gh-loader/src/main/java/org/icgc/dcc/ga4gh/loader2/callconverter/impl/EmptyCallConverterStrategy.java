b5ce8093085e9c685c3494b6caa5d22f1a5db7fe
rtisma <rtisma@gmail.com>
2017-05-08 10:25:23 -0400

    updated EmptyCallConverterStrategy

--- ga4gh-loader/src/main/java/org/icgc/dcc/ga4gh/loader2/callconverter/impl/EmptyCallConverterStrategy.java
+++ ga4gh-loader/src/main/java/org/icgc/dcc/ga4gh/loader2/callconverter/impl/EmptyCallConverterStrategy.java
@@ -12,14 +12,26 @@ import static org.icgc.dcc.ga4gh.loader2.callconverter.CallConverterStrategy.che
 
 public class EmptyCallConverterStrategy implements CallConverterStrategy {
 
-  private static final List<EsCall> EMPTY_CALL_LIST = ImmutableList.of();
   private static final int EXPECTED_NUMBER_OF_CALLS = 0;
+  private static final double DEFAULT_DUMMY_GENOTYPE_LIKELIHOOD = 0.0;
+  private static final boolean DEFAULT_DUMMY_GENOTYPE_PHASED = false;
+  private static final List<Integer> DEFAULT_DUMMY_NON_REFERENCE_ALLELES = ImmutableList.of(-1);
 
   @Override
   public List<EsCall> convert(EsCall.EsCallBuilder callBuilder, VariantContext variantContext) {
     val actualNumCalls = variantContext.getGenotypes().size();
     checkNumCalls(actualNumCalls, EXPECTED_NUMBER_OF_CALLS);
-    return EMPTY_CALL_LIST;
+    return buildDummyCall(callBuilder, variantContext);
+  }
+
+  private static List<EsCall>  buildDummyCall(EsCall.EsCallBuilder callBuilder, VariantContext variantContext){
+    val commonInfoMap = variantContext.getCommonInfo().getAttributes();
+    return ImmutableList.of(callBuilder
+        .info(commonInfoMap)
+        .genotypeLikelihood(DEFAULT_DUMMY_GENOTYPE_LIKELIHOOD)
+        .isGenotypePhased(DEFAULT_DUMMY_GENOTYPE_PHASED)
+        .nonReferenceAlleles(DEFAULT_DUMMY_NON_REFERENCE_ALLELES)
+        .build());
   }
 
 }
