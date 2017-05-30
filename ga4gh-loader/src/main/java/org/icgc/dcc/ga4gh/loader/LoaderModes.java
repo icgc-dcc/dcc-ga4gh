b1779907fe1e120b4cd0cdd3f2190706f0be1195
rtisma <rtisma@gmail.com>
2017-05-30 08:37:19 -0400

    refactored LoaderModes logic

--- ga4gh-loader/src/main/java/org/icgc/dcc/ga4gh/loader/LoaderModes.java
+++ ga4gh-loader/src/main/java/org/icgc/dcc/ga4gh/loader/LoaderModes.java
@@ -2,7 +2,7 @@ package org.icgc.dcc.ga4gh.loader;
 
 import lombok.val;
 
-import static com.google.common.base.Preconditions.checkArgument;
+import static org.icgc.dcc.common.core.util.stream.Streams.stream;
 
 public enum LoaderModes {
   AGGREGATE_ONLY(1), INDEX_ONLY_BASIC(2), FULLY_LOAD(3), INDEX_ONLY_SPECIAL(4);
@@ -14,15 +14,8 @@ public enum LoaderModes {
   }
 
   public static LoaderModes parseLoaderMode(final int inputMode) {
-    boolean found = false;
-    for (val loaderMode : values()) {
-      if (loaderMode.getModeId() == inputMode) {
-        found = true;
-        return loaderMode;
-      }
-    }
-    checkArgument(found, "The inputMode {} does not exist for LoaderModes", inputMode);
-    return LoaderModes.FULLY_LOAD; // Should never be reached
+    val mode = stream(values()).filter(l -> l.getModeId()==inputMode).findFirst();
+    return mode.orElseThrow(() -> new IllegalArgumentException(String.format("The inputMode {} does not exist for LoaderModes", inputMode)));
   }
 
   public int getModeId() {
